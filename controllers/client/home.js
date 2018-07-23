8f048db810b91b951bd91a645d84bf213932ff2a
Carlos Power <chefpower25@gmail.com>
2018-07-23 16:14:16 -0400

    Fixing home.html

--- controllers/client/home.js
+++ controllers/client/home.js
@@ -1,14 +1,3 @@
 octoDex.controller("homeCtrl", function ($scope, $rootScope, $location, $http, $window, $timeout) {
     console.log("This is the homeCtrl");
-    $scope.people;
-
-    $http({
-        method: 'GET',
-        url: '/api/get.php'
-    }).then(function (response) {
-        console.log(response);
-        $scope.people = response.data;
-    }, function (response) {
-        console.log(response.data,response.status);
-    });
 });
