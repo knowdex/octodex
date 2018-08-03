dea32bdf90ac5f7d842e4a6aca75f4b5ff622c2a
Carlos Power <chefpower25@gmail.com>
2018-08-03 10:26:17 -0400

    working with login

--- controllers/front/loginCtrl.js
+++ controllers/front/loginCtrl.js
@@ -1,3 +1,28 @@
-octoDex.controller("loginCtrl", function () {
+octoDex.controller("loginCtrl", function ($scope,$http,appConfig) {
+    $scope.userLogin = function(){
 
+        var user_data= 'user_email=' + $scope.oldEmail + '&user_password=' + $scope.oldPassword;
+        console.log(user_data);
+
+        $http({
+            method: 'POST',
+            url: appConfig.backendURL + '/static/api/login.php/',
+            data: user_data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) { //success
+            console.log(data);
+            if ( data.trim() === 'correct') {
+                window.location.href = '#/index';
+            } else {
+                console.log("Username or password is incorrect");
+            }
+        });
+
+        //Resets form
+        // $scope.newPhoto = null;
+        // $scope.newProduct = null;
+        // $scope.newDescription = null;
+        // $scope.newPrice = null;
+
+    }
 });
